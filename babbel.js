c259a41dae20b5d54705a41ed07e537846b5e404
Yoann Joly <yoann.joly12@gmail.com>
2018-12-20 12:42:48 +0100

    js

--- babbel.js
+++ babbel.js
@@ -1,15 +1,15 @@
-var images=["/home/laura/Documents/Simplon/Projets/p2_babbel/p2_babbel/medias/carousel/carousel1.jpg","/home/laura/Documents/Simplon/Projets/p2_babbel/p2_babbel/medias/carousel/carousel2.jpg","/home/laura/Documents/Simplon/Projets/p2_babbel/p2_babbel/medias/carousel/carousel3.jpg","/home/laura/Documents/Simplon/Projets/p2_babbel/p2_babbel/medias/carousel/carousel4.jpg"];
+var images=["medias/carousel/carousel1.jpg","/medias/carousel/carousel2.jpg","medias/carousel/carousel3.jpg","medias/carousel/carousel4.jpg"];
 
 i=0;
 var next=function(){
     var slider=document.getElementById("slider");
     i++;
     if (i<images.length){
-        slider.src=images[i]
+        slider.src=images[i];
         }
     else{
-        i==0
-    };
+        i==0;
+    }
 };
 
 i=0;
@@ -17,9 +17,9 @@ var prev=function(){
     var slider=document.getElementById("slider");
     i--;
     if (i<images.length){
-        slider.src=images[i]
+        slider.src=images[i];
         }
     else{
-        i==0
-    };
+        i==0;
+    }
 };
