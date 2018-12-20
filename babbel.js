aab73cba702c478162139fc9b274558a561d7e5e
laurabello <laurabello710@gmail.com>
2018-12-20 11:05:27 +0100

    slide

---
+++ babbel.js
@@ -0,0 +1,13 @@
+var images=["/home/laura/Documents/Simplon/Projets/p2_babbel/p2_babbel/medias/carousel/carousel1.jpg","/home/laura/Documents/Simplon/Projets/p2_babbel/p2_babbel/medias/carousel/carousel2.jpg","/home/laura/Documents/Simplon/Projets/p2_babbel/p2_babbel/medias/carousel/carousel3.jpg","/home/laura/Documents/Simplon/Projets/p2_babbel/p2_babbel/medias/carousel/carousel4.jpg"];
+
+i=0;
+var next=function(){
+    var slider=document.getElementById("slider");
+    i++;
+    if (i<images.length){
+        slider.src=images[i]
+        }
+    else{
+        i==0
+    };
+};
