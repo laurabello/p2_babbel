7aafbf732de0b9594846d74fcf8fe163748505a2
rocquet cecile <cecilerocquet@sfr.fr>
2018-12-20 15:59:05 +0100

    mise au point

--- babbel.js
+++ babbel.js
@@ -1,25 +1,21 @@
 var images=["medias/carousel/carousel1.jpg","medias/carousel/carousel2.jpg","medias/carousel/carousel3.jpg","medias/carousel/carousel4.jpg"];
 
-i=0;
+var i=0;
+
 var next=function(){
     var slider=document.getElementById("slider");
-    i++;
-    if (i<images.length){
-        slider.src=images[i]
-        }
-    else{
-        i==0
-    };
+    i=i+1
+    if(i == 4){
+        i = 0
+    }
+    slider.src = images[i]
 };
 
-i=0;
 var prev=function(){
     var slider=document.getElementById("slider");
-    i--;
-    if (i<images.length){
-        slider.src=images[i]
-        }
-    else{
-        i==0
-    };
+    i=i-1
+    if(i <0 ){
+        i=3
+    }
+    slider.src = images[i]
 };
