76d925b9bae6db5a15d67e4eabcff184860f58c7
AymericAr <arteroaymeric@gmail.com>
2018-12-20 14:51:29 +0100

    com

--- babbel.js
+++ babbel.js
@@ -1,25 +1,33 @@
-var images=["/home/laura/Documents/Simplon/Projets/p2_babbel/p2_babbel/medias/carousel/carousel1.jpg","/home/laura/Documents/Simplon/Projets/p2_babbel/p2_babbel/medias/carousel/carousel2.jpg","/home/laura/Documents/Simplon/Projets/p2_babbel/p2_babbel/medias/carousel/carousel3.jpg","/home/laura/Documents/Simplon/Projets/p2_babbel/p2_babbel/medias/carousel/carousel4.jpg"];
+var images=[
+    "/home/aymeric/Desktop/remplacent/babbeltest/p2_babbel/medias/carousel/carousel1.jpg","/home/aymeric/Desktop/remplacent/babbeltest/p2_babbel/medias/carousel/carousel2.jpg","/home/aymeric/Desktop/remplacent/babbeltest/p2_babbel/medias/carousel/carousel3.jpg","/home/aymeric/Desktop/remplacent/babbeltest/p2_babbel/medias/carousel/carousel4.jpg"];
+
 
 i=0;
-var next=function(){
+var next = function() {
+
     var slider=document.getElementById("slider");
     i++;
-    if (i<images.length){
-        slider.src=images[i]
-        }
+    if (i < images.length){
+        slider.src=images[i];
+    }
     else{
-        i==0
+        slider.src=images[i];
+        i=0;
     };
 };
 
-i=0;
-var prev=function(){
+var prev = function(){
     var slider=document.getElementById("slider");
     i--;
-    if (i<images.length){
+    if (i < images.length) {
         slider.src=images[i]
         }
+    if (i=-1){
+        slider.src=images[i];
+        i=5
+    }
     else{
-        i==0
+        slider.src=images[i];
+        i = images.length-1;
     };
-};
+}
